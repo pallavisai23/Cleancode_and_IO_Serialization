f6de802206c312f3a76b382b4302a9b42318da7f
pallavisai23 <54880008+pallavisai23@users.noreply.github.com>
2020-02-20 20:25:34 +0530

    Add files via upload

---
+++ ConstructionPriceEstimator/src/main/java/com/pallavi/ConstructionPriceEstimator/App.java
@@ -0,0 +1,13 @@
+package com.pallavi.ConstructionPriceEstimator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    }
+}
